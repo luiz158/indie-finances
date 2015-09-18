8ad20eaa2ddef71e51b06d0c081adf13ded10a52
Marcao <marcofognog@gmail.com>
2015-09-18 10:37:06 -0300

    Display values as money

--- app/models/finances/dre_attributes.rb
+++ app/models/finances/dre_attributes.rb
@@ -8,21 +8,69 @@ module Finances
 
     def self.dre_fields
       ["name",
-      "pagseguro",
-      "adwords",
-      "facebook_ad",
-      "facebook_paid_publications",
-      "zencoder_dollars",
-      "zencoder_reais",
-      "heroku_dollars",
-      "heroku_reais",
-      "amazon_dollars",
-      "amazon_reais",
-      "paypal"]
+       "pagseguro",
+       "adwords",
+       "facebook_ad",
+       "facebook_paid_publications",
+       "zencoder_dollars",
+       "zencoder_reais",
+       "heroku_dollars",
+       "heroku_reais",
+       "amazon_dollars",
+       "amazon_reais",
+       "paypal"]
     end
 
-    self.dre_fields.each do |field_name|
-      delegate(field_name.to_sym, { :to => :"@resource" })
+    def name
+      to_money_unit(@resource.name)
+    end
+
+    def pagseguro
+      to_money_unit(@resource.pagseguro)
+    end
+
+    def adwords
+      to_money_unit(@resource.adwords)
+    end
+
+    def facebook_ad
+      to_money_unit(@resource.facebook_ad)
+    end
+
+    def facebook_paid_publications
+      to_money_unit(@resource.facebook_paid_publications)
+    end
+
+    def zencoder_dollars
+      to_money_unit(@resource.zencoder_dollars)
+    end
+
+    def zencoder_reais
+      to_money_unit(@resource.zencoder_reais)
+    end
+
+    def heroku_dollars
+      to_money_unit(@resource.heroku_dollars)
+    end
+
+    def heroku_reais
+      to_money_unit(@resource.heroku_reais)
+    end
+
+    def amazon_dollars
+      to_money_unit(@resource.amazon_dollars)
+    end
+
+    def amazon_reais
+      to_money_unit(@resource.amazon_reais)
+    end
+
+    def paypal
+      to_money_unit(@resource.paypal)
+    end
+
+    def to_money_unit(value)
+      value.to_f / 100
     end
   end
 
