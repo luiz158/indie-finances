e0d09ca3271c083b0bf306ffbd2bf253579124b1
Marcao <marcofognog@gmail.com>
2015-09-22 22:19:56 -0300

    Improve first spec:
    Time is not passed as a parameter, but computed at run time.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gemspec
 gem "jquery-rails", "~> 3.1.1"
 gem "rspec"
 gem 'rspec-rails'
+gem 'delorean'
 # Declare any dependencies that are still in development here instead of in
 # your gemspec. These might include edge Rails or gems from your path or
 # Git. Remember to move these dependencies to your gemspec before releasing
--- spec/integration/balance_spec.rb
+++ spec/integration/balance_spec.rb
@@ -4,7 +4,10 @@ module Finances
   describe "Balance" do
     it "builds the balance for given entries" do
       params1 = Hash.new
-      params1[:entry] = { credit: 27, debit: 0, date: DateTime.parse("19-02-2015"), description: "Resultado de Novembro" }
+      params1[:entry] = { credit: 27, debit: 0, description: "Resultado de Novembro" }
+
+      Delorean.time_travel_to("09-22-2015")
+
       entry1 = CreateEntryPage.new(params1)
       entry1.save
 
