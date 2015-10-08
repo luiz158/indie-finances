644a96b513ee8de58773824ca89a4b2eb0fe8800
Marcao <marcofognog@gmail.com>
2015-10-08 19:33:05 -0300

    Fix length of description field in reports table.

--- db/migrate/20151006124209_create_reports_table.rb
+++ db/migrate/20151006124209_create_reports_table.rb
@@ -2,7 +2,7 @@ class CreateReportsTable < ActiveRecord::Migration
   def change
     create_table :finances_reports do |t|
       t.string :name
-      t.string :description
+      t.text :description
       t.text :snapshot
       t.string :title
       t.timestamps
