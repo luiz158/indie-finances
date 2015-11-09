86e14b648072b7d2909dfc8109300b4dd276d713
Marcao <marcofognog@gmail.com>
2015-11-09 09:14:15 -0200

    Improve bumpv

--- bumpv.sh
+++ bumpv.sh
@@ -1,4 +1,5 @@
-local current=ruby -e "$: << '.'; require 'lib/finances/version.rb'; puts Finances::VERSION"
+current=$(ruby -e "$: << '.'; require 'lib/finances/version.rb'; puts Finances::VERSION")
+
+echo "Bumpin to: $current"
 git tag $current
 git push --tags origin master
-
