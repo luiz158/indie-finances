f9d202cf999deca9e4d1eb41ec735a8f5903694c
Marcao <marcofognog@gmail.com>
2016-11-19 21:04:51 -0200

    Make sure the entries are loaded in proper order.

--- app/models/finances/index_entry_page.rb
+++ app/models/finances/index_entry_page.rb
@@ -7,7 +7,7 @@ module Finances
       lines = []
       patrimony = 0
 
-      Entry.all.each do |entry|
+      Entry.order("date asc").each do |entry|
         patrimony += entry.amount
         lines << { date: entry.date.strftime("%d-%m-%Y"), amount: to_money_unit(entry.amount), description: entry.description, id: entry.id, kind: "entry" }
         lines << { date: entry.date.strftime("%d-%m-%Y"), amount: to_money_unit(patrimony), description: "", kind: "patrimony" }
